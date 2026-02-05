00e3b188e9f74f1eb7a7f5202ccfb3d02f4c2ce8
yKoutl <raulquintanazinc@gmail.com>
2026-02-05 17:15:48 -0500

    configuracion del Planet Bot en el App

--- src/api/helper/url-auth.js
+++ src/api/helper/url-auth.js
@@ -1,8 +1,6 @@
 export const urlBaseAuth = "http://localhost:3000/api/auth";
-export const urlBase = "http://192.168.18.8:3000/api/auth";
-export const urlLevels = "http://192.168.18.8:3000/api/levels";
-export const urlUser = "http://192.168.18.8:3000/api/users";
-export const urlPartners = "http://192.168.18.8:3000/api/partners";
-export const urlInduction = "http://192.168.18.8:3000/api/induction";
-export const urlPrograms = "http://192.168.18.8:3000/api/programs";
-export const urlRewards = "http://192.168.18.8:3000/api/rewards";
+export const urlBase = "http://192.168.1.3:3000/api/auth";
+export const urlLevels = "http://192.168.1.3:3000/api/levels";
+export const urlUser = "http://192.168.1.3:3000/api/users";
+export const urlPartners = "http://192.168.1.3:3000/api/partners";
+export const urlInduction = "http://192.168.1.3:3000/api/induction";
